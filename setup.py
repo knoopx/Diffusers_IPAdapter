52bcb9a2ab881ae58468e0963009667862bbb4be
Víctor Martínez <knoopx@gmail.com>
2024-06-10 21:04:05 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from setuptools import setup, find_packages
+
+setup(name='ip_adapter',
+      version='0.1',
+      description='implementation of the IPAdapter models for HF Diffusers',
+      author='cubiq',
+      url='https://github.com/cubiq/Diffusers_IPAdapter',
+      packages=find_packages(where='ip_adapter'),
+      package_dir={"": "ip_adapter"}
+)
